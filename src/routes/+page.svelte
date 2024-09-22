824c380938cc9d6bd9c58e08b59f59ccce31ff1d
Github1234fab <fabien.marceau@gmail.com>
2024-09-22 13:24:06 +0200

    oeoeo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,39 +4,6 @@
         import UpLoadJsonAdvertisement from "../components/UpLoadJsonAdvertisement.svelte";
         import "../routes/styles.css";
 
-        import { onMount } from "svelte";
-
-        onMount(() => {
-                window.OneSignal = window.OneSignal || [];
-                OneSignal.push(function () {
-                        OneSignal.init({
-                                appId: "YOUR_APP_ID",
-                                notifyButton: {
-                                        enable: true,
-                                        size: "medium",
-                                        theme: "default",
-                                        position: "bottom-right",
-                                        offset: { bottom: "0px", right: "0px" },
-                                        showCredit: false,
-                                        text: {
-                                                "tip.state.unsubscribed": "Subscribe to notifications",
-                                                "tip.state.subscribed": "You're subscribed to notifications",
-                                                "tip.state.blocked": "You've blocked notifications",
-                                                "message.prenotify": "Click to subscribe to notifications",
-                                                "message.action.subscribed": "Thanks for subscribing!",
-                                                "message.action.resubscribed": "You're subscribed to notifications",
-                                                "message.action.unsubscribed": "You won't receive notifications again",
-                                                "dialog.main.title": "Manage Site Notifications",
-                                                "dialog.main.button.subscribe": "SUBSCRIBE",
-                                                "dialog.main.button.unsubscribe": "UNSUBSCRIBE",
-                                                "dialog.blocked.title": "Unblock Notifications",
-                                                "dialog.blocked.message": "Follow these instructions to allow notifications:",
-                                        },
-                                },
-                        });
-                });
-        });
-</script>
 
 <main>
         <Calendar />
