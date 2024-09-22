0f143d81eeb7d38d68e68e69efaef4ffbc871fb5
Github1234fab <fabien.marceau@gmail.com>
2024-09-22 08:45:48 +0200

    uuu

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,21 @@
 // place files you want to import through the `$lib` alias in this folder.
+OneSignal.push(function () {
+        // Vérifier l'état de permission
+        OneSignal.isPushNotificationsEnabled(function (isEnabled) {
+                if (isEnabled) {
+                        console.log("Notifications sont activées.");
+                } else {
+                        console.log("Notifications ne sont pas activées.");
+                }
+        });
+
+        // Écouter l'obtention des tokens
+        OneSignal.on("subscriptionChange", function (isSubscribed) {
+                console.log("L'abonnement a changé:", isSubscribed);
+                if (isSubscribed) {
+                        OneSignal.getUserId(function (userId) {
+                                console.log("Le token de l'utilisateur est:", userId);
+                        });
+                }
+        });
+});
