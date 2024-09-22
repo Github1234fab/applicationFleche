3930b695c6667c16a935053f2e7a15a58c3a73c6
Github1234fab <fabien.marceau@gmail.com>
2024-09-22 08:26:31 +0200

    kkkk

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,2 +1,24 @@
 
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+
+OneSignal.push(function () {
+        // Vérifier l'état de permission
+        OneSignal.isPushNotificationsEnabled(function (isEnabled) {
+                if (isEnabled) {
+                        console.log("Notifications sont activées.");
+                } else {
+                        console.log("Notifications ne sont pas activées.");
+                }
+        });
+
+        // Écouter l'obtention des tokens
+        OneSignal.on("subscriptionChange", function (isSubscribed) {
+                console.log("L'abonnement a changé:", isSubscribed);
+                if (isSubscribed) {
+                        OneSignal.getUserId(function (userId) {
+                                console.log("Le token de l'utilisateur est:", userId);
+                        });
+                }
+        });
+});
